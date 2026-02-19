497805080aeb45a6ed1e8e8955162f1c9f5eba25
peroserog <peroserog@unal.edu.co>
2026-02-18 22:13:15 -0500

    clase usuario completada. cambio de nombre a InfoUsuarios por Inscripcion. tiene sentido. dejalo asi, que sino afecta todo el programa

--- src/usuario.java
+++ src/usuario.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class usuario {
+    private String nombre;
+    private String email;
+    private List<Inscripcion> misInscripciones;
+
+    usuario(String nombre, String email){
+        this.setNombre(nombre);
+        this.setEmail(email);
+        this.misInscripciones = new ArrayList<>();
+    }
+    public void agregarInscripcion(Inscripcion insc) {
+        this.misInscripciones.add(insc);
+    }
     
+
+    public String getNombre(){return nombre;}
+    public String getEmail(){return email;}
+
+    public void setNombre(String nombre){this.nombre = nombre;
+    }
+    public void setEmail(String email){this.email = email;}
 }
+

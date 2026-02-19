3f043911bc2501755a1feef5fcfff677ae8dd691
miguelarangov <miarangova@unal.edu.co>
2026-02-19 13:56:31 -0500

    3 clases completadas esperando metodo

---
+++ src/Estado.java
@@ -0,0 +1,5 @@
+public enum Estado {
+    ACTIVA,
+    CANCELADA,
+    FINALIZADA 
+}
--- src/Incripcion.java
+++ src/Incripcion.java
@@ -1,3 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Incripcion {
-    
+    public String Fecha;
+    private Estado estado;
+    private double progreso;
+    private List<Leccion> leccionesCompletadas;
+
+    public Incripcion (String fecha) {
+        this.Fecha = fecha;
+        this.estado = Estado.ACTIVA;
+        this.progreso = 0.0;
+        this.leccionesCompletadas = new ArrayList<>();
+    }
+        public void completarLeccion(Leccion leccion) {
+            if(!leccionesCompletadas.contains(leccion)){
+                leccionesCompletadas.add(leccion);
+                actualizarProgreso();
+            }
+        }
+
 }
